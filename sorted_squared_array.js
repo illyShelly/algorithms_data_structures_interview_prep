f7ede6a9ffe590586dea04669047eca5b2038d27
Ilona Sellenberkova <ilona.sell@gmail.com>
2021-10-31 16:54:16 +0100

    Easy add another sortedSquaredArray solution

--- sorted_squared_array.js
+++ sorted_squared_array.js
@@ -1,6 +1,6 @@
-// O(n) time | O(n) space - n is the length of the input arr
+// O(nlog(n)) time (sorting operations) | O(n) space - length of the input arr
+
 function sortedSquaredArray(array) {
-  // Write your code here.
   let sorted = [];
 
   array.forEach((num) => {
@@ -10,3 +10,30 @@ function sortedSquaredArray(array) {
   return sorted.sort((a, b) => a - b);
 }
 
+
+// Algoexpert solution
+// O(n) time | O(n) space
+function sortedSquaredArray(array) {
+  // array is sorted! in ascending order e.x.: [-10, -5, 0, 6, 12]
+  const sorted = [];
+  // const sorted = new Array(array.length).fill(0);
+  let leftIdx = 0;
+  let rightIdx = array.length - 1;
+
+  // iterate from right to left -> higher number
+  for (let i = array.length - 1; i >= 0; i--) {
+    const leftValue = array[leftIdx];
+    const rightValue = array[rightIdx];
+
+    // compare left (-10) and right (10) values (their absolute value)
+    // then add to the array to the left or right and increase/decrease its left/right idx
+    if (Math.abs(leftValue) > Math.abs(rightValue)) {
+      sorted[i] = leftValue * leftValue;  // add to last position leftIdx value (0.)
+      leftIdx ++;
+    } else {
+      sorted[i] = rightValue * rightValue;
+      rightIdx --;
+    }
+}
+  return sorted;
+}
